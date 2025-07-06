8e6d5cc2b2c8a93c6ed0120850bf58df6c39d73a
KumJungMin <rmawjdals@naver.com>
2025-07-07 00:23:00 +0900

    chore(lib): change compile setting

--- packages/styleguide-lib/vite.config.ts
+++ packages/styleguide-lib/vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
     vue(),
-    dts({
-      insertTypesEntry: true,
-      rollupTypes: true,
-    }),
   ],
   build: {
     lib: {
