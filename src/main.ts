57e2fdfd9a7b024bd1365f344d808b8a174e1589
KumJungMin <rmawjdals@naver.com>
2025-07-06 21:14:07 +0900

    refactor: 기존 파일 구조 정리
    - 기존 단일 앱 구조에서 모노레포로 완전 이전
    - 불필요한 파일들 삭제 및 정리
    - 새로운 패키지 구조에 맞게 파일 이동 완료

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp, h } from 'vue'
-import App from './App.vue'
-
-const app = createApp(App)
-app.mount('#app')
-
-// iframe 미리보기를 위해 Vue를 window에 노출
-window.Vue = { createApp, h } 
